55f1c4bb0875ed061d7014d5158f19f2798cfb88
Nicolas Di Costanzo <dicostanzo.nicolas@gmail.com>
2024-02-20 12:01:16 +0100

    app modification

--- redux-ts/src/index.tsx
+++ redux-ts/src/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import RepositoriesList from './components/RepositoriesList';
+import { Provider } from 'react-redux';
+import { store } from './state';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+  <div>
+    <h1>Search for a Package</h1>
+    <RepositoriesList />
+  </div>
+ </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
